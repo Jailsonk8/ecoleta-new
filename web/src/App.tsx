fdbb60391886cca395aba7d81e1285763bc51cf1
Jailsonk8 <jailsonk8@hotmail.com>
2020-07-03 19:28:29 -0300

    server e web finsh ecoleta-nlw

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './Routes';
+
 function App() {
-  return (
-      <h1>Hello World !</h1>
+
+  return ( 
+    <Routes />
   );
 }
 
