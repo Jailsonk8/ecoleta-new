337e23d2027a5d85b2322a07cbc2ee2ed1c7196b
Jailsonk8 <jalsonk8@hotmail.com>
2020-07-15 12:47:46 -0300

    some changes

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,18 +1,18 @@
-import path from 'path';
+import path from "path";
 
 module.exports = {
-    client: 'pg',
-    version: '12.3',
-    connection: {
-        host : 'db',
-        user : 'postgres',
-        password : 'postgres',
-        database : 'db_ecoleta'
-    },
-    migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-    },
-    seeds: {
-        directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-    },
-}; 
+  client: "pg",
+  version: "12.3",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "postgres",
+    database: "db_ecoleta",
+  },
+  migrations: {
+    directory: path.resolve(__dirname, "src", "database", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "src", "database", "seeds"),
+  },
+};
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,14 +1,14 @@
-import knex from 'knex';
+import knex from "knex";
 
 const connection = knex({
-    client: 'pg',
-    version: '12.3',
-    connection: {
-        host : 'db',
-        user : 'postgres',
-        password : 'postgres',
-        database : 'db_ecoleta'
-    },
+  client: "pg",
+  version: "12.3",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "postgres",
+    database: "db_ecoleta",
+  },
 });
 
 export default connection;
